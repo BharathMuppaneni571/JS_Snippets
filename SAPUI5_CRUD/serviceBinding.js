1af5d7ab9bc0deef801e17afd4fd5effa92480d5
Bharath Muppaneni <muppanenibharath571@gmail.com>
2020-12-30 15:47:41 +0530

    created seperate proj so deleting this one.

--- SAPUI5_CRUD/serviceBinding.js
+++
@@ -1,5 +0,0 @@
-function initModel() {
-	var sUrl = "/northwind/V2/(S(ws3f4ioul3yiqbrwuordp5zx))/OData/OData.svc/";
-	var oModel = new sap.ui.model.odata.ODataModel(sUrl, true);
-	sap.ui.getCore().setModel(oModel);
-}
--- SAPUI5_CRUD/webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"namespace/CRUD20191221/test/unit/controller/View1.controller"
-], function () {
-	"use strict";
-});
